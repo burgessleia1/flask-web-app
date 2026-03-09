a0a96371f1579689045905669f9ea2b0ce208cc8
Leia Burgess <burgessleia1@gmail.com>
2026-03-09 09:52:57 -0600

    Finalize Flask Web App: populate all files, templates, static JS, auth, and models

---
+++ website/views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+@login_required
+def home():
+    return render_template('home.html', user=current_user)
+
+@views.route('/add-note', methods=['POST'])
+@login_required
+def add_note():
+    note = request.form.get('note')
+    if len(note) < 1:
+        flash('Note is too short!', category='error')
+    else:
+        new_note = Note(data=note, user_id=current_user.id)
+        db.session.add(new_note)
+        db.session.commit()
+        flash('Note added!', category='success')
+    return redirect(url_for('views.home'))
